bd998a98fcf1b801e3d4b88940f90d2a8c7b1602
bartr <bartr@microsoft.com>
2020-02-23 00:57:05 -0600

    updated quick start

--- kv-cli-auth/src/main/java/com/keyvault/quickstart/App.java
+++ kv-cli-auth/src/main/java/com/keyvault/quickstart/App.java
@@ -1,35 +1,51 @@
 package com.keyvault.quickstart;
 
-import java.io.IOException;
 import com.microsoft.azure.credentials.*;
 import com.microsoft.azure.keyvault.*;
 import com.microsoft.azure.keyvault.models.*;
+import com.microsoft.azure.keyvault.requests.*;
 
 public class App {
-
     public static void main(String[] args) throws InterruptedException, IllegalArgumentException {
 
         String keyVaultName = System.getenv("jkv_Name");
 
         if (keyVaultName == null || keyVaultName.isBlank()) {
-            System.out.println("Set the KEY_VAULT_NAME environment variable");
+            System.out.println("Set the jkv_Name environment variable");
             System.exit(-1);
         }
 
-        String kvUri = "https://" + keyVaultName.trim() + ".vault.azure.net";
-
-        System.out.println(kvUri);
+        // build key vault URL
+        String kvUrl = "https://" + keyVaultName.trim() + ".vault.azure.net";
+        System.out.println(kvUrl);
 
         try {
+            // get credentials from Azure CLI cache
             AzureTokenCredentials cred = AzureCliCredentials.create();
 
+            // create Key Vault client
             KeyVaultClient kvClient = new KeyVaultClient(cred);
 
-            SecretBundle secret = kvClient.getSecret(kvUri, "mySecret");
-
+            // get mySecret
+            SecretBundle secret = kvClient.getSecret(kvUrl, "mySecret");
             System.out.println(secret.value());
 
-        } catch (IOException ex) {
+            // create myNewSecret
+            SetSecretRequest req = new SetSecretRequest.Builder(kvUrl, "myNewSecret", "My new secret value").build();
+            kvClient.setSecret(req);
+
+            // get myNewSecret
+            SecretBundle newSecret = kvClient.getSecret(kvUrl, "myNewSecret");
+            System.out.println(newSecret.value());
+
+            // delete myNewSecret
+            kvClient.deleteSecret(kvUrl, "myNewSecret");
+
+            // newSecret should be null
+            newSecret = kvClient.getSecret(kvUrl, "myNewSecret");
+            System.out.println(newSecret == null);
+
+        } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
 
